2de6e5708438b0439671e47ff3235d41ebda138f
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-20 14:18:38 -0800

    create service

---
+++ src/service.js
@@ -0,0 +1,11 @@
+const CLIENT_ID = "OKe9Ls6EVh7UDFW56w95y7UKITeHooWJP72UaZBa";
+
+const SECRET = "QaEbVIlHqV3MrHxK8znsdjIoCDwdluERIvwYNvW7bZ8QH9rxe2Ga5D3U5aSNSDPnkpag1GkaBBRSR8QnQP8BLXDJxBGyeYgnc3HrQfJFh4YsmRt5apnzpfN9FCgsFiWV";
+
+const GRANT_TYPE = 'password';
+
+export default {
+    getAuthToken: ()=>{
+        
+    }
+};
