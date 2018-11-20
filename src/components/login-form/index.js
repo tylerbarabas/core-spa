7782a47474d3e42eb51e6e6b5f0f8e3dbaaf2c1d
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-20 08:32:26 -0800

    listener for the form

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './index.scss'
 
 export default class LoginForm extends React.Component {
+
+    handleSubmit(e){
+        e.preventDefault();
+        console.log('handleSubmit');
+    }
+
     render(){
         return(
             <div className="section">
@@ -9,15 +15,17 @@ export default class LoginForm extends React.Component {
                         <div className="column box is-one-fifth is-offset-two-fifths is-radiusless has-text-centered login-form">
                             <img className="logo" src="logo.png" alt="RevCascade Logo" />
                             <strong className="slogan">Automating eCommerce.</strong>
-                            <input className="is-one-quarter input login-fields" type="text" placeholder="Email Address" />
-                            <input className="is-one-quarter input login-fields" type="password" placeholder="Password" />
-                            <div className="login-remember-me">
-                                <label className="checkbox is-pulled-left">
-                                    <input type="checkbox" className="is-pulled-left" />
-                                    Remember me?
-                                </label>
-                            </div>
-                            <button type="button" className="button is-primary login-button">Sign In</button>
+                            <form onSubmit={this.handleSubmit.bind(this)}>
+                                <input className="is-one-quarter input login-fields" type="text" placeholder="Email Address" />
+                                <input className="is-one-quarter input login-fields" type="password" placeholder="Password" />
+                                <div className="login-remember-me">
+                                    <label className="checkbox is-pulled-left">
+                                        <input type="checkbox" className="is-pulled-left" />
+                                        Remember me?
+                                    </label>
+                                </div>
+                                <button type="submit" className="button is-primary login-button">Sign In</button>
+                            </form>
                         </div>
                     </div>
             </div>
