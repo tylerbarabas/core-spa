dda5b57648bfb68c4105c8bb88187075395c8154
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-19 18:43:28 -0800

    login form component

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -9,6 +9,15 @@ export default class LoginForm extends React.Component {
                         <div className="column box is-one-fifth is-offset-two-fifths is-radiusless has-text-centered login-form">
                             <img className="logo" src="logo.png" />
                             <strong>Automating eCommerce.</strong>
+                            <input className="is-one-quarter input login-fields" type="text" placeholder="Email Address" />
+                            <input className="is-one-quarter input login-fields" type="password" placeholder="Password" />
+                            <div className="login-remember-me">
+                                <label className="checkbox is-pulled-left">
+                                    <input type="checkbox" className="is-pulled-left" />
+                                    Remember me
+                                </label>
+                            </div>
+                            <button type="button" className="button is-primary login-button">Sign In</button>
                         </div>
                     </div>
             </div>
