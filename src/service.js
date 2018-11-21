56b5fa1968e3e1c955227d0ff3b7505f6c1f9d42
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-20 17:38:56 -0800

    fetch function for retreving auth token

--- src/service.js
+++ src/service.js
@@ -1,12 +1,25 @@
 const CLIENT_ID = "OKe9Ls6EVh7UDFW56w95y7UKITeHooWJP72UaZBa";
-
 const SECRET = "QaEbVIlHqV3MrHxK8znsdjIoCDwdluERIvwYNvW7bZ8QH9rxe2Ga5D3U5aSNSDPnkpag1GkaBBRSR8QnQP8BLXDJxBGyeYgnc3HrQfJFh4YsmRt5apnzpfN9FCgsFiWV";
-
 const GRANT_TYPE = 'password';
+
+const HOST = 'https://api-staging.revcascade.com';
+const uri_authToken = `${HOST}/auth/token/`;
 
 export default {
     getAuthToken: async () => {
-        let req = await fetch('disney.com');
-        return req;
+        let res = await fetch(uri_authToken, {  
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            mode: 'no-cors',
+            body: JSON.stringify({
+                'client_id': CLIENT_ID,
+                'client_secret': SECRET,
+                'grant_type': GRANT_TYPE
+            })
+        });
+        console.log('res?', res);
+        return res;
     }
 };
