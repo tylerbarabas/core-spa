d94fb6bae27598b8146ab776b82aff9f3c4a5a8c
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-21 12:47:39 -0800

    re-simplify App container since auth will no longer be firing on app load

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,39 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from '../home'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { requestToken } from '../../modules/token'
 import 'bulma'
 import './base.scss'
 
-class App extends React.Component{
-    componentDidMount(){
-        this.props.requestToken();
-    }
+const App = () => (
+  <div>
+    <main>
+      <Route exact path="/" component={Home} />
+    </main>
+  </div>
+)
 
-    render(){
-        return (
-            <div>
-                <main>
-                    <Route exact path="/" component={Home} />
-                </main>
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = () => ({})
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-        requestToken
-    },
-    dispatch
-  )
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default App
