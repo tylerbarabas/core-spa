85de14b06e9049820006ce378e4268e531226626
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-26 15:59:34 -0600

    custom component extends Route from react-router but adds protection if not authenticated

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import PrivateRoute from '../../components/private-route'
 import Login from '../login'
+import Dashboard from '../dashboard'
 import 'bulma'
 import 'animate.css'
 import './base.scss'
@@ -8,7 +10,8 @@ import './base.scss'
 const App = () => (
   <div>
     <main>
-      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path="/" component={Dashboard} />
     </main>
   </div>
 )
