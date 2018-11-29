2da9cccda6d9bc39594576d4f68fac792d53b9d9
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-29 14:02:12 -0600

    create context selector

---
+++ src/components/context-selector/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class ContextSelector extends React.Component {
+    render(){
+        return (
+            <h1 style={{margin: '0 auto'}}>Context Selector</h1>
+        )
+    }
+}
--- src/components/dashboard-main/index.js
+++ src/components/dashboard-main/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default class DashCenter extends React.Component {
     render(){
-        return (<h1>DASH CENTER</h1>)
+        return (
+            <h1 style={{margin: '0 auto'}}>Dash Center</h1>
+        )
     }
 }
