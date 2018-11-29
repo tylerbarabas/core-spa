48b328e9d872ff0e54d0473806991cee9f218413
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-29 15:48:28 -0600

    handle context logic inside of dashboard container

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -3,29 +3,44 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { logout } from '../../modules/auth'
 import { getMyUser } from '../../modules/user'
+import ContextSelector from '../../components/context-selector'
+import DashboardMain from '../../components/dashboard-main'
+import TopBar from '../../components/top-bar'
 
 class Dashboard extends React.Component {
     componentDidMount(){
         this.props.getMyUser();
     }
 
-    logout(){
-        this.props.logout();
-    }
-
     componentDidUpdate(){
-        if (this.props.user.isError) this.logout();
+        if (this.props.user.isError) this.props.logout();
     }
 
     isUser(){
-        return (this.props.user.id === null) ? 'is-invisible' : '';
+        return (this.props.user.id === null) ? {display: 'none'} : {};
+    }
+
+    isContext(){
+        let { user } = this.props;
+        let template = (<DashboardMain />);
+
+        console.log('user', user);
+        if (user.id === null) {
+            template = null;
+        } else if (user.isRevcascade) {
+            template = (<ContextSelector brands={'all'} retailers={'all'} />);
+        } else {
+            //fetch contexts, if more than one, feed them to the context selector, else leave template unmodified
+            template = (<ContextSelector brands={['mybrand']} retailers={['myretailer']} />);
+        }
+        return template;
     }
 
     render(){
         return (
-            <div className={this.isUser()}>
-                This is the dashboard.
-                <div onClick={this.logout.bind(this)}>LOG OUT</div>
+            <div style={this.isUser()}>
+                <TopBar logout={this.props.logout} />
+                { this.isContext() }
             </div>
         ) 
     }
