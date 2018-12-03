eac130d4b59b675e000fa7d63db3da88e840fcf8
Tyler Barabas <tylerbarabas@gmail.com>
2018-12-03 15:59:33 -0800

    finish with big-loading

--- src/components/big-loading/index.js
+++ src/components/big-loading/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import './index.scss'
 
 export default class BigLoading extends React.Component {
     render(){
         return (
-            <div>
-                Hello
+            <div className="big-loading columns">
+                <div className="column is-one-fifth is-offset-two-fifths animated fadeInDown has-text-centered">
+                    <FontAwesomeIcon icon={faSpinner} spin className="loading-spinner" />
+                    <p className="loading-text">Loading...</p>
+                </div>
             </div>
         )
     }
