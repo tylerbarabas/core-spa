2798260d69cda14f01d882d9a7751e9faed110bb
Tyler Barabas <tylerbarabas@gmail.com>
2018-12-04 12:27:47 -0800

    faTh icon styles

--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTh } from '@fortawesome/free-solid-svg-icons'
 import './index.scss'
 
 export default class TopBar extends React.Component {
     render(){
         return (
             <div className="top-bar animated fadeInDown">
+                <FontAwesomeIcon icon={faTh} /> 
                 <div className="logout" onClick={this.props.logout.bind(this)}>Log out</div>
             </div>
         )
