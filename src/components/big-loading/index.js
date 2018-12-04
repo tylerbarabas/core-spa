dbeb83dd71cdc6f82ce2cde6ffa370dbdf163027
Tyler Barabas <tylerbarabas@gmail.com>
2018-12-04 12:10:43 -0800

    fix up big-loading

--- src/components/big-loading/index.js
+++ src/components/big-loading/index.js
@@ -7,7 +7,7 @@ export default class BigLoading extends React.Component {
     render(){
         return (
             <div className="big-loading columns">
-                <div className="column is-one-fifth is-offset-two-fifths animated fadeInDown has-text-centered">
+                <div className="column is-one-fifth is-offset-two-fifths animated fadeIn has-text-centered">
                     <FontAwesomeIcon icon={faSpinner} spin className="loading-spinner" />
                     <p className="loading-text">{this.props.msg || 'Loading...'}</p>
                 </div>
