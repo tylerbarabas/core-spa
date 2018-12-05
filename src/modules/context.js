fe1a1be022f4942f3f3b53996bd9263c598a42d5
Tyler Barabas <tylerbarabas@gmail.com>
2018-12-04 22:47:08 -0600

    bring in new module for context

---
+++ src/modules/context.js
@@ -0,0 +1,29 @@
+export const CONTEXT_SELECTED = 'context/CONTEXT_SELECTED'
+
+const initialState = {
+    id: null,
+    name: null,
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case CONTEXT_SELECTED:
+            return {
+                ...state,
+                id: action.id,
+                name: action.name
+            }
+        default:
+            return state
+    }
+}
+
+export const selectContext = ({ id, name }) => {
+  return dispatch => {
+    dispatch({
+      type: CONTEXT_SELECTED,
+      id,
+      name
+    })
+  }
+};
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import auth from './auth'
 import user from './user'
+import context from './context'
 
 export default combineReducers({
   auth,
-  user
+  user,
+  context
 })
