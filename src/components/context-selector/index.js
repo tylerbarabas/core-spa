a9c1a354e9a66de2e6d0b1a9f39c57f8bfa13efd
Tyler Barabas <tylerbarabas@gmail.com>
2018-12-04 22:03:04 -0600

    contect selector is working with autocomplete

--- src/components/context-selector/index.js
+++ src/components/context-selector/index.js
@@ -1,13 +1,60 @@
 import React from 'react'
+import Select from 'react-select'
 import './index.scss'
 
 export default class ContextSelector extends React.Component {
+  constructor(props){
+    super(props);
+    this.brands = this.props.brands.map( b => {
+      return { value: b.id, label: b.name, stateProp: 'selectedBrand' };
+    });
+
+    this.retailers = this.props.retailers.map( r => {
+      return { value: r.id, label: r.name, stateProp: 'selectedRetailer' };
+    });
+
+    this.state = {
+      selectedBrand: null,
+      selectedRetailer: null,
+    };
+  }
+
+  handleChange( selected, options ){
+    let { stateProp, value } = selected;
+    this.setState({ [stateProp]: selected });
+  }
+
   render(){
+    let { brands, retailers } = this;
+    let { selectedBrand, selectedRetailer } = this.state;
+
     return (
       <div className="section">
         <div className="columns">
           <div className="column is-one-third is-offset-one-third animated fadeInDown context-selector">
             <h1 className="title">Choose context.</h1>
+            <div className="form-control">
+              <label htmlFor="select-brand">Vendor</label>
+              <Select
+                id="select-brand"
+                className="select-context"
+                value={selectedBrand}
+                onChange={this.handleChange.bind(this)}
+                options={brands}
+                placeholder="Select vendor..."
+              /> 
+            </div>
+            <div className="form-control">
+              <label htmlFor="select-retailer">Retailer</label>
+              <Select
+                id="select-retailer"
+                className="select-context"
+                value={selectedRetailer}
+                onChange={this.handleChange.bind(this)}
+                options={retailers}
+                placeholder="Select retailer..."
+              /> 
+            </div>
           </div>
         </div>
       </div> 
