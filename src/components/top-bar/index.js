f806da61a020e6ba0e9830a0dc5f7ad8c1746788
Tyler Barabas <tylerbarabas@gmail.com>
2019-01-07 00:06:14 -0600

    create placeholder for the buttons

--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -4,12 +4,19 @@ import { faTh } from '@fortawesome/free-solid-svg-icons'
 import './index.scss'
 
 export default class TopBar extends React.Component {
+    getButtons(){
+      return(
+        <div className="buttons">Buttons!</div>
+      );
+    }
+
     render(){
         return (
             <div className="top-bar animated fadeInDown">
                 <FontAwesomeIcon icon={faTh} />
                 <img src="rc_circle.png" className ="rc-circle" alt="RevCascade Logo" />
                 <strong className="app-name">{ this.props.name || "" }</strong>
+                {this.getButtons()}
                 <div className="logout is-pulled-right" onClick={this.props.logout.bind(this)}>Log out</div>
             </div>
         )
