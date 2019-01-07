e900fb40ef1059bf9ddc45b5293689520a285a78
Tyler Barabas <tylerbarabas@gmail.com>
2019-01-06 23:29:46 -0600

    allow top bar to take a prop name

--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -8,7 +8,8 @@ export default class TopBar extends React.Component {
         return (
             <div className="top-bar animated fadeInDown">
                 <FontAwesomeIcon icon={faTh} />
-                <img src="rc_circle.png" className ="rc-circle" alt="RevCascade Logo" /> 
+                <img src="rc_circle.png" className ="rc-circle" alt="RevCascade Logo" />
+                { this.props.name || "" }
                 <div className="logout is-pulled-right" onClick={this.props.logout.bind(this)}>Log out</div>
             </div>
         )
