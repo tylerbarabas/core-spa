61b959a5b45a322ece439af1dce612c7a67f63e9
Tyler Barabas <tylerbarabas@gmail.com>
2019-01-12 21:25:56 -0600

    redo the navbar the bulma way

--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -10,13 +10,12 @@ export default class TopBar extends React.Component {
     let template = []
     for (let i=0;i<buttons.length;i++){
       let selected = ''
-      if (i===2) selected = 'selected'
       let b = buttons[i]
-      template.push(<div key={b.name} className={`nav-button ${selected}`}>{b.name}</div>)
+      template.push(<a key={b.name} className='navbar-item'>{b.name}</a>)
     }
 
     return(
-      <div className="nav-buttons">
+      <div className="navbar-start">
         {template}
       </div>
     )
@@ -26,15 +25,32 @@ export default class TopBar extends React.Component {
     let { firstName } = this.props.user
     let { isAuthenticated, logout } = this.props
     return (
-      <div className={`top-bar animated fadeInDown${(!isAuthenticated) ? ' is-hidden':''}`}>
-        <FontAwesomeIcon icon={faTh} />
-        <img src="rc_circle.png" className ="rc-circle" alt="RevCascade Logo" />
-        <strong className="app-name">{ this.props.name || '' }</strong>
-        {this.getButtons()}
-        <div className="logout is-pulled-right" onClick={logout.bind(this)}>
-                Hi, {firstName} <FontAwesomeIcon icon={faChevronDown} />
+      <nav className={`navbar animated fadeInDown${(!isAuthenticated) ? ' is-hidden':''}`} role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item">
+            <FontAwesomeIcon icon={faTh} />
+            <img src="rc_circle.png" className="rc-circle" />
+            Catalog
+          </a>
         </div>
-      </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            {this.getButtons()}
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link is-arrowless">
+                Hi, {firstName} <FontAwesomeIcon icon={faChevronDown} />
+              </a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item" onClick={logout}>
+                  Logout
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
