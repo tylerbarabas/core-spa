85810aa58f997d894c712cb38575e6a538d31520
Tyler Barabas <tylerbarabas@gmail.com>
2019-01-12 22:06:19 -0600

    move context logic in to all private routes

--- src/components/private-route/index.js
+++ src/components/private-route/index.js
@@ -3,22 +3,48 @@ import PropTypes from 'prop-types'
 import { Route } from 'react-router-dom'
 import Login from '../../containers/login'
 import Service from '../../service'
+import ContextSelector from '../context-selector'
+import BigLoading from '../../components/big-loading'
 
-export default function PrivateRoute ({ component: Component, isAuthenticated, setIsAuthenticated, ...rest}) {
-  return ( <Route {...rest} render={() => {
-    if (isAuthenticated === true) {
-      return (<Component {...rest} />)
-    } else if (Service.isValidCookie() === true){
-      setIsAuthenticated(true)
-      return null
-    } else {
-      return (<Login />)
+export default class PrivateRoute extends React.Component {
+  isContext(Component){
+    let { user, context, selectContext, ...rest } = this.props
+    let template = (<Component {...rest} />);
+
+    if (user.id === null) {
+      template = <BigLoading msg={'Retrieving user data...'} />
+    } else if (context.id === null) {
+      let combined = user.retailers.concat(user.brands)
+      if (combined.length > 1) {
+        template = (<ContextSelector brands={user.brands} retailers={user.retailers} selectContext={selectContext} />)
+      } else if (combined.length === 1) {
+        selectContext(combined[0])
+      }
     }
-  }} /> )
+
+    return template
+  }
+
+  render(){
+    let { isAuthenticated, setIsAuthenticated, component, ...rest } = this.props;
+    return ( <Route {...rest} render={() => {
+        if (isAuthenticated === true) {
+          return (<div>{ this.isContext(component) }</div>)
+
+        } else if (Service.isValidCookie() === true){
+          setIsAuthenticated(true)
+          return null
+        } else {
+          return (<Login />)
+        }
+      }} /> )
+  }
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.func,
   isAuthenticated: PropTypes.bool,
   setIsAuthenticated: PropTypes.func,
+  context: PropTypes.object,
+  user: PropTypes.object,
 }
--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -1,36 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ContextSelector from '../../components/context-selector'
-import BigLoading from '../../components/big-loading'
 
 export default class Dashboard extends React.Component {
   componentDidUpdate(){
     if (this.props.user.isError) this.props.logout()
   }
 
-  isContext(){
-    let { user, context } = this.props
-    let DashboardMain = this.props.dashboard
-    let template = (<DashboardMain context={context} />)
-
-    if (user.id === null) {
-      template = <BigLoading msg={'Retrieving user data...'} />
-    } else if (context.id === null) {
-      let combined = user.retailers.concat(user.brands)
-      if (combined.length > 1) {
-        template = (<ContextSelector brands={user.brands} retailers={user.retailers} selectContext={this.props.selectContext} />)
-      } else if (combined.length === 1) {
-        this.props.selectContext(combined[0])
-      }
-    }
-
-    return template
-  }
-
   render(){
     return (
       <div>
-        { this.isContext() }
+        Dashers
       </div>
     ) 
   }
