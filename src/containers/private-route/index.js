40134e8f62d347859167c34857cf4381742bbb00
Tyler Barabas <tylerbarabas@gmail.com>
2019-01-12 23:10:06 -0600

    now that private-route is depending on so many props, lets make it a container component

--- src/containers/private-route/index.js
+++ src/containers/private-route/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
-import Login from '../../containers/login'
+import { setIsAuthenticated } from 'core-spa/src/modules/auth'
+import { getMyUser } from 'core-spa/src/modules/user'
+import { selectContext } from 'core-spa/src/modules/context'
+
+import Login from '../login'
 import Service from '../../service'
-import ContextSelector from '../context-selector'
+import ContextSelector from '../../components/context-selector'
 import BigLoading from '../../components/big-loading'
 
-export default class PrivateRoute extends React.Component {
+class PrivateRoute extends React.Component {
   isContext(Component){
     let { user, context, selectContext, ...rest } = this.props
     let template = (<Component {...rest} />)
@@ -26,7 +32,9 @@ export default class PrivateRoute extends React.Component {
   }
 
   render(){
-    let { isAuthenticated, setIsAuthenticated, component, ...rest } = this.props
+    let { setIsAuthenticated, component, ...rest } = this.props
+    let { isAuthenticated } = this.props.auth
+
     return ( <Route {...rest} render={() => {
       if (isAuthenticated === true) {
         return (<div>{ this.isContext(component) }</div>)
@@ -48,3 +56,24 @@ PrivateRoute.propTypes = {
   context: PropTypes.object,
   user: PropTypes.object,
 }
+
+const mapStateToProps = ({ auth, user, context }) => ({ 
+  auth,
+  user,
+  context,
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getMyUser,
+      setIsAuthenticated,
+      selectContext,
+    },
+    dispatch
+  )
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PrivateRoute)
