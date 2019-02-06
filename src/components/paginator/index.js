f7f856a1385e4a5fa8820cbbcb41efb935999cd5
Tyler Barabas <tylerbarabas@gmail.com>
2019-02-06 10:59:34 -0600

    create paginator

---
+++ src/components/paginator/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './index.scss'
+
+export default class Paginator extends React.Component {
+  render(){
+    return (
+      <div className="paginator-container columns">
+        <div className="column">
+        <nav className="pagination is-right" role="navigation" aria-label="pagination">
+          <a className="pagination-previous">Previous</a>
+          <a className="pagination-next">Next page</a>
+          <ul className="pagination-list">
+            <li><a className="pagination-link" aria-label="Goto page 1">1</a></li>
+            <li><span className="pagination-ellipsis">&hellip;</span></li>
+            <li><a className="pagination-link" aria-label="Goto page 45">45</a></li>
+            <li><a className="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+            <li><a className="pagination-link" aria-label="Goto page 47">47</a></li>
+            <li><span className="pagination-ellipsis">&hellip;</span></li>
+            <li><a className="pagination-link" aria-label="Goto page 86">86</a></li>
+          </ul>
+        </nav>
+        </div>
+      </div>
+    )
+  }
+}
+
+Paginator.propTypes = {
+  count: PropTypes.number
+}
