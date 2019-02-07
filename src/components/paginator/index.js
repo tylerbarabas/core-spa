3f84e9f56af7913b94d036e74c1ca3fcd21dca00
Tyler Barabas <tylerbarabas@gmail.com>
2019-02-07 12:01:46 -0600

    getPaginationList needs to adapt to all kinds of stuff

--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -10,11 +10,37 @@ export default class Paginator extends React.Component {
     }
   }
 
-  render(){
-    let { count } = this.props
+  getPaginationList(){
     const l = 25
+    let { count } = this.props
     let { currentPage } = this.state
     let totalPages = Math.ceil(count / l)
+    let template = []
+
+    console.log('totalPages', totalPages)
+
+    if ( totalPages > 6 ) {
+      template.push(
+        <span>
+          <li><a className={`pagination-link${(currentPage===1)?' is-current':''}`} aria-label="Goto page 1">1</a></li>
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+          <li><a className="pagination-link" aria-label="Goto page 45">45</a></li>
+          <li><a className="pagination-link" aria-label="Page 46" aria-current="page">46</a></li>
+          <li><a className="pagination-link" aria-label="Goto page 47">47</a></li>
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+          <li><a className="pagination-link" aria-label={`Goto page ${totalPages}`}>{totalPages}</a></li> 
+        </span>
+      ) 
+    } else {
+      for ( let i=1; i <= totalPages; i+=1 ){
+        template.push(<li><a className={`pagination-link${(currentPage===i)?' is-current':''}`} aria-label={`Goto page ${i}`}>{i}</a></li>)
+      }
+    }
+
+    return template
+  }
+
+  render(){
     return (
       <div className="paginator-container columns">
         <div className="column">
@@ -22,13 +48,7 @@ export default class Paginator extends React.Component {
           <a className="pagination-previous">Previous</a>
           <a className="pagination-next">Next page</a>
           <ul className="pagination-list">
-            <li><a className={`pagination-link${(currentPage===1)?' is-current':''}`} aria-label="Goto page 1">1</a></li>
-            <li><span className="pagination-ellipsis">&hellip;</span></li>
-            <li><a className="pagination-link" aria-label="Goto page 45">45</a></li>
-            <li><a className="pagination-link" aria-label="Page 46" aria-current="page">46</a></li>
-            <li><a className="pagination-link" aria-label="Goto page 47">47</a></li>
-            <li><span className="pagination-ellipsis">&hellip;</span></li>
-            <li><a className="pagination-link" aria-label={`Goto page ${totalPages}`}>{totalPages}</a></li>
+            {this.getPaginationList()}
           </ul>
         </nav>
         </div>
