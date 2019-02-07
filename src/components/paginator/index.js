2348125e7e7f7640821b2afd9e7259ea47c0d622
Tyler Barabas <tylerbarabas@gmail.com>
2019-02-06 19:25:13 -0600

    create local state for paginator, calculate totalPages based on count

--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types'
 import './index.scss'
 
 export default class Paginator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 1
+    }
+  }
+
   render(){
+    let { count } = this.props
+    const l = 25
+    let { currentPage } = this.state
+    let totalPages = Math.ceil(count / l)
     return (
       <div className="paginator-container columns">
         <div className="column">
@@ -11,13 +22,13 @@ export default class Paginator extends React.Component {
           <a className="pagination-previous">Previous</a>
           <a className="pagination-next">Next page</a>
           <ul className="pagination-list">
-            <li><a className="pagination-link" aria-label="Goto page 1">1</a></li>
+            <li><a className={`pagination-link${(currentPage===1)?' is-current':''}`} aria-label="Goto page 1">1</a></li>
             <li><span className="pagination-ellipsis">&hellip;</span></li>
             <li><a className="pagination-link" aria-label="Goto page 45">45</a></li>
-            <li><a className="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+            <li><a className="pagination-link" aria-label="Page 46" aria-current="page">46</a></li>
             <li><a className="pagination-link" aria-label="Goto page 47">47</a></li>
             <li><span className="pagination-ellipsis">&hellip;</span></li>
-            <li><a className="pagination-link" aria-label="Goto page 86">86</a></li>
+            <li><a className="pagination-link" aria-label={`Goto page ${totalPages}`}>{totalPages}</a></li>
           </ul>
         </nav>
         </div>
