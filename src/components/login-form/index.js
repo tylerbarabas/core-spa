a24c0a8daa3a65e3375582f6b61662f0f7c050fa
Tyler Barabas <tylerbarabas@gmail.com>
2019-02-11 15:11:59 -0600

    login page

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -38,7 +38,7 @@ export default class LoginForm extends React.Component {
 
   animateContainer(){
     const { errorMsg } = this.props
-    const staticClasses = 'column is-one-third is-offset-one-third is-radiusless animated login-form'
+    const staticClasses = 'columns is-centered is-radiusless animated login-form'
     let animClass = ''
     if ( errorMsg !== null ) animClass = 'pulse'
     if ( this.isInitialLoad ) {
@@ -62,9 +62,9 @@ export default class LoginForm extends React.Component {
     let { errorMsg, isRequesting } = this.props
     return(
       <div className="section">
-        <div className="columns">
-          <div className={this.animateContainer()}>
-            <img className="logo" src="/logo.png" alt="RevCascade Logo" />
+        <div className={this.animateContainer()}>
+          <div className="column is-half">
+          <div><img className="logo" src="/logo.png" alt="RevCascade Logo" /></div>
             <div className={'message '+(errorMsg?'error-msg':'')}>{ this.getMessage() }</div>
             <form onSubmit={this.handleSubmit.bind(this)}>
               <div className="field">                            
