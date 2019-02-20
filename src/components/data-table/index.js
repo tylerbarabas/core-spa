51d73e61b29994ee2f416682bd50f2999e870fdc
Tyler Barabas <tylerbarabas@gmail.com>
2019-02-19 20:18:12 -0600

    build out the data-table component

--- src/components/data-table/index.js
+++ src/components/data-table/index.js
@@ -4,14 +4,59 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 
 export default class DataTable extends React.Component {
+  getTableHeader(){
+    let { columns } = this.props
+    let template = []
+    for (let i=0;i<columns.length;i+=1) {
+      let c = columns[i]
+      let style = ( i === 0 ) ? 'first' : ''
+      template.push( <th className={style}>{c.title}</th> )
+    }
+    return template
+  }
+
+  getTableBody(){
+    let { data } = this.props
+    let template = []
+    for (let i=0;i<data.length;i+=1){
+      template.push(<tr>{this.getTableRow(i)}</tr>)
+    }
+
+    return template
+  }
+
+  getTableRow(index){
+    let { columns, data } = this.props
+    let template = []
+    for (let i=0;i<columns.length;i+=1){
+      let c = columns[i]
+      let d = data[index]
+      template.push(<td>{d[c.index]}</td>)
+    }
+    return template
+  }
+
   render(){
-    let { myData } = this.props.data[0]
     return (
-      <div>{myData}</div>
+      <div className="columns">
+        <div className="column">
+          <table className="table data-table">
+            <thead>
+              <tr>
+                {this.getTableHeader()}
+              </tr>
+            </thead>
+            <tbody>
+              { this.getTableBody() }
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
   }
 }
 
 DataTable.propTypes = {
   data: PropTypes.array,
+  columns: PropTypes.array,
 }
