e8dcb102c14beeea052e9dae7dd7f6d54b7eadd4
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-01 11:37:03 -0600

    paginator only appears if we have more than 1 page

--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -103,8 +103,9 @@ export default class Paginator extends React.Component {
   render(){
     let { page } = this.props
     let limit = this.getTotalPages() - 1
+    let display = (limit < 1) ? {display: 'none'} : {display: ''}
     return (
-      <div className="paginator-container column is-full">
+      <div className="paginator-container column is-full" style={display}>
         <nav className="pagination is-right" role="navigation" aria-label="pagination">
           <a className="pagination-previous" onClick={this.previousClicked.bind(this)} disabled={(page<2)}>Previous</a>
           <a className="pagination-next" onClick={this.nextClicked.bind(this)} disabled={(page > limit)}>Next page</a>
