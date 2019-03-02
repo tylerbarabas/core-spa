82195367edcf3230468cf4e028f4da5467b7dfa6
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-02 15:25:54 -0600

    radio-filter

---
+++ src/components/radio-filter/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class RadioFilter extends React.Component {
+  constructor(){
+    super()
+    this.checked = null
+  }
+
+  radioSelected(e){
+    let { action, filterKey } = this.props
+    this.checked = e.target.value
+    action(this.checked, filterKey)
+  }
+
+  resetFilter(){
+    this.checked = null
+  }
+
+  getOptions(){
+    let { options, filterKey } = this.props
+    let template = []
+    for (let i=0;i<options.length;i+=1) {
+      let o = options[i]
+      template.push(
+        <div key={i} className="filter-option">
+          <input type="radio"
+            key={i}
+            id={`filter-${filterKey}-${i}`}
+            value={o.value}
+            onChange={this.radioSelected.bind(this)}
+            checked={(this.checked.indexOf(o.value) !== -1)}
+          /> <label htmlFor={`filter-${filterKey}-${i}`}>{o.display}</label>
+        </div>
+      )
+    }
+
+    return template
+  }
+
+  render(){
+    let { name } = this.props
+    return (
+      <div className="filter">
+        <strong>{name}</strong>
+        <div className="reset-all" onClick={this.resetFilter.bind(this)}>Reset All</div>
+        { this.getOptions() }
+      </div>
+    )
+  }
+}
+
+RadioFilter.propTypes = {
+  filterKey: PropTypes.string,
+  name: PropTypes.string,
+  options: PropTypes.array,
+  action: PropTypes.func,
+}
