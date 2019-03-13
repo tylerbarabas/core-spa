8bbb2e82c6f3396d31f5c755a3bc5aa2e5a5ad87
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-12 22:31:04 -0500

    get the right modal entrance

--- src/components/card-modal/index.js
+++ src/components/card-modal/index.js
@@ -19,7 +19,7 @@ export default class CardModal extends React.Component {
     return (
       <div className={`card-modal modal ${(isActive)?'is-active':''}`}>
         <div className="modal-background" />
-        <div className="modal-card animated fadeInDown">
+        <div className="modal-card animated flipInX">
           <header className="modal-card-head">
             <p className="modal-card-title">{modalTitle}</p>
             <button
