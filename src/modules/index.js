5cb8c2725297cc3bf3bac224d5dafa7bd9cb891e
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-14 15:02:42 -0500

    combine redux modules

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import user from './user'
 import context from './context'
+import notification from './notification'
 
 export default combineReducers({
   auth,
   user,
-  context
+  context,
+  notification,
 })
