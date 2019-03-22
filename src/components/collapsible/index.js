519bcb389c4eb4cb35711acc62222f28d1c621b2
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-22 10:49:51 -0500

    toggle active state of collapsible

--- src/components/collapsible/index.js
+++ src/components/collapsible/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import './index.scss'
 
 export default class Collapsible extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      isActive: false
+    }
+  }
+
+  clicked(e){
+    console.log('clicked', e)
+    let { isActive } = this.state
+    this.setState({isActive: !isActive})
+  }
+
   render(){
     let { innerTemplate } = this.props
+    let { isActive } = this.state
+    let ac = ( isActive ) ? 'is-active' : ''
+
     return (
-      <div className="collapsible">
-        { innerTemplate() }
+      <div onClick={this.clicked.bind(this)} className={`collapsible ${ac}`}>
+        <FontAwesomeIcon icon={faAngleDown} />
+       { innerTemplate() }
       </div>
     )
   }
