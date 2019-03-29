85b8c35864fcd19f07646eee018e612a72e6307a
Tyler Barabas <tylerbarabas@gmail.com>
2019-03-29 12:56:42 -0500

    bring in small loading component

---
+++ src/components/small-loading/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import './index.scss'
+
+export default class SmallLoading extends React.Component {
+  render(){
+    return (
+      <div className="small-loading has-text-centered">
+        <FontAwesomeIcon icon={faSpinner} spin className="loading-spinner" />
+        <p className="loading-text">{this.props.msg || 'Loading...'}</p>
+      </div>
+    )
+  }
+}
+
+SmallLoading.propTypes = {
+  msg: PropTypes.string
+}
