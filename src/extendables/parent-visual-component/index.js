d94556cc5c40e14b6d3862da886357ab5836be3f
Tyler Barabas <tylerbarabas@gmail.com>
2019-05-20 12:57:58 -0500

    copyStringToClipboard

--- src/extendables/parent-visual-component/index.js
+++ src/extendables/parent-visual-component/index.js
@@ -94,6 +94,27 @@ export default class ParentVisualComponent extends React.Component {
 
     return `https://res.cloudinary.com/revcascade/${modStr}${url}`
   }
+
+  /**
+    Copy a string to the clipboard.
+    @param str - any string
+  **/
+  copyStringToClipboard (str) {
+    // Create new element
+    var el = document.createElement('textarea')
+    // Set value (string to be copied)
+    el.value = str
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '')
+    el.style = {position: 'absolute', left: '-9999px'}
+    document.body.appendChild(el)
+    // Select text inside element
+    el.select()
+    // Copy text to clipboard
+    document.execCommand('copy')
+    // Remove temporary element
+    document.body.removeChild(el)
+  }
 }
 
 ParentVisualComponent.propTypes = {
