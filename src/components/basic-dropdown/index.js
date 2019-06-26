f06e98a6a5503a29f90aecb76e64682cc5d5e241
Tyler Barabas <tylerbarabas@gmail.com>
2019-06-26 06:16:54 -0500

    basic dropdown

---
+++ src/components/basic-dropdown/index.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import './index.scss'
+
+export default class BasicDropdown extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      value: null,
+      isActive: false,
+    }
+  }
+
+  componentDidMount(){
+    let { items } = this.props
+    if (
+      !Array.isArray(items)
+      && items.length < 1
+    ) items[0] = null
+    this.setState({value: items[0]})
+  }
+
+  getItems(){
+    let { items } = this.props
+    let { value, isActive } = this.state
+    let template = []
+    for(let i=0;i<items.length;i+=1) {
+      let it = items[i]
+      template.push(
+        <a className={`dropdown-item is-pulled-left${(it === value)?' is-active':''}`} onClick={this.selectItem.bind(this, it)}>
+          {it}
+        </a>
+      )
+    }
+    return template
+  }
+
+  selectItem(value){
+    this.setState({value})
+  }
+
+  toggleDropdown(){
+    let { isActive } = this.state
+    console.log('look', isActive)
+    this.setState({isActive: !isActive})
+  }
+
+  render(){
+    let { value, isActive } = this.state
+    return (
+      <div className={`basic-dropdown dropdown${(isActive)?' is-active':''}`}>
+        <div className="dropdown-trigger" onClick={this.toggleDropdown.bind(this)}>
+          <button className="button is-pulled-left" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>{value}</span>
+            <span className={`icon is-small${(isActive)?' active':''}`}>
+              <FontAwesomeIcon icon={faAngleDown} />
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {this.getItems()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+BasicDropdown.propTypes = {
+  value: PropTypes.string,
+  items: PropTypes.array,
+}
