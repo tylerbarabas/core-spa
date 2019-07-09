92a6ea8cec870118ee260ed83421122a6875246b
Tyler Barabas <tylerbarabas@gmail.com>
2019-07-08 17:56:13 -0700

    hammer out the uri massage in getOrders

--- src/service.js
+++ src/service.js
@@ -32,7 +32,7 @@ const uri_getInventoryProducts = fullPath('/v1/retailers/:id/inventory/?limit=15
 const uri_elasticSearch = fullPath('/v1/:rb/:id/variants/search/?limit=15')
 const uri_productsOptions = fullPath('/v1/:rb/:id/attributes/?filterable_attributes_only=1')
 const uri_ordersSummary = fullPath('/v1/:rb/:id/orders-summary/')
-const uri_orders = fullPath('/v1/retailers/502/orders/?short=1&page=1&search_order_number=3&limit=15&status=open%2Cpartial&order_by=-is_priority%2C-created_at')
+const uri_orders = fullPath('/v1/:rb/:id/orders/?short=1&page=1&:sb=:st&limit=15')
 
 let Auth = {
   accessToken: null,
@@ -211,8 +211,8 @@ export default {
 
     return data
   },
-  getOrders: async ( id, rb = 'retailers' ) => {
-    let uri = `${uri_orders.replace(/:rb/, rb).replace(/:id/, id)}`
+  getOrders: async ( id, rb = 'retailers', searchTerm = '', searchBy = '' ) => {
+    let uri = `${uri_orders.replace(/:rb/, rb).replace(/:id/, id).replace(/:sb/,searchBy).replace(/:st/, searchTerm)}`
     let res = await superFetch(uri)
     let data = false
     if (res.ok){
