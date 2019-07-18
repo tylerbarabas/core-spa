e8f7e77761f278b043032d8d790d77cf1a740e9f
Tyler Barabas <tylerbarabas@gmail.com>
2019-07-17 17:33:18 -0700

    flesh out the expandy-card component

--- src/components/expandy-card/index.js
+++ src/components/expandy-card/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import './index.scss'
 
 export default class ExpandyCard extends React.Component {
   render(){
+    let { topArea, bottomArea } = this.props
+    let isExpanded = false
+
     return (
-      This is expandy card.
+      <div className="expandy-card">
+        <FontAwesomeIcon icon={faPlus} className={`card-plus${(isExpanded)?' is-expanded':''}`} />
+        <FontAwesomeIcon icon={faMinus} className={`card-minus${(isExpanded)?' is-expanded':''}`} />
+        <div className="top-area">
+          {topArea()}
+        </div>
+        <div className="bottom-area">
+          {bottomArea()}
+        </div>
+      </div>
     )
   }
 }
