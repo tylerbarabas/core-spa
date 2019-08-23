e1564d74b1db7e6004d4c93f554ceaf515498c68
Tyler Barabas <tylerbarabas@gmail.com>
2019-08-23 16:11:07 -0700

    bring back style sheet for dash table

--- src/components/dash-table/index.js
+++ src/components/dash-table/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import './index.scss'
 
 export default class DashTable extends React.Component {
   getRows(){
@@ -10,25 +11,31 @@ export default class DashTable extends React.Component {
       let r = rows[i]
       template.push(
         <div className="columns is-mobile is-desktop has-background-white has-radius m-b-lg p-sm" key={`dash-table-row-${i}`}>
-              <div className="column">
-                <Link to={r.href} className="is-size-5 has-text-weight-bold has-text-primary">
-                  {r.title}
-                </Link>
-                <p className="is-size-6">
-                  {r.description}
-                </p>
-              </div>
-              <div className="column is-narrow">
-                <p className="count">
-                  {r.count}
-                </p>
-              </div>
+          <div className="column">
+            <Link to={r.href} className="is-size-5 has-text-weight-bold has-text-primary">
+              {r.title}
+            </Link>
+            <p className="is-size-6">
+              {r.description}
+            </p>
+          </div>
+          <div className="column is-narrow is-vcentered">
+            <p className="count is-size-5 has-text-centered has-background-grey-lighter">
+              {this.getCount(r.count)}
+            </p>
+          </div>
         </div>
       )
     }
     return template
   }
 
+  getCount(count){
+    if (count === 'View') return (
+      <span className="is-size-6 has-text-black">View</span>
+    )
+    return count
+  }
 
   render(){
     return (
