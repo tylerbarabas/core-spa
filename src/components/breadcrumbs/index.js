4dd46b3c87004364defbeb7420bf01de80ea37af
Tyler Barabas <tylerbarabas@gmail.com>
2019-08-23 17:52:55 -0700

    linter

--- src/components/breadcrumbs/index.js
+++ src/components/breadcrumbs/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import key from './key.json'
@@ -30,7 +29,4 @@ export default class Breadcrumbs extends React.Component {
       </div>
     )
   }
-}
-
-Breadcrumbs.propTypes = {
 }
